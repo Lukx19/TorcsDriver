6ff43cc82e3a0640eaebe458c0962b032cb167db
Lukas Jelinek <lukx19@gmail.com>
2017-11-28 13:52:17 +0100

    Implementing Neat model

---
+++ neat_model.py
@@ -0,0 +1,92 @@
+from model import Model
+import numpy as np
+import pickle
+import math
+
+
+class NeatModel(Model):
+    def __init__(self, model_file, result_file):
+        with open(model_file, 'rb') as f:
+            self.net = pickle.load(f)
+            self.net.reset()
+        self.result_file = result_file
+        self.predictions = 0
+        self.offroad_penalty = 0
+        self.cumul_speed = 0
+        self._current_lap_time = 0
+        self._last_laps_accumulated_time = 0
+
+        self.results = []
+
+    # def __del__(self):
+    #     """Try to save data before instance is destroyed."""
+    #     self.saveResults()
+
+    def saveResults(self):
+        with open(self.result_file, 'w') as f:
+            f.write(
+                "time, raced_distance, distance_from_start, damage, offroad_penalty, avg_speed\n")
+            for r in self.results:
+                f.write("{}, {}, {}, {}, {}, {}\n".format(*r))
+
+    def _projectSpeed(self,speed_x, speed_y, angle):
+        velocity = np.angle(speed_x + 1j * speed_y, True),np.sqrt(speed_x ** 2 + speed_y ** 2)
+        return velocity[1] * math.cos(math.radians(angle - velocity[0]))
+
+    def predict(self, state):
+        input = self._to_input_array(state, smaller=True)
+        if np.isnan(input).any():
+            print('NaN INPUTS!!!')
+            return
+        output = self.net.activate(input)
+        self.acceleration = output[0]
+        self.steering = output[1]
+        self.breaking = output[2]
+        self.predictions += 1
+
+        if self._current_lap_time > state.current_lap_time:
+            self._last_laps_accumulated_time += state.last_lap_time
+        self._current_lap_time = state.current_lap_time
+
+        self.offroad_penalty += (max(0,
+                                     math.fabs(state.distance_from_center) - 0.95)) ** 2
+        projected_speed = self._projectSpeed(
+            state.speed_x, state.speed_y, state.angle)
+
+        self.cumul_speed += projected_speed
+
+        if self.predictions % 100 == 0:
+            self.results.append([
+                self._current_lap_time + self._last_laps_accumulated_time,
+                state.distance_raced,
+                state.distance_from_start,
+                state.damage,
+                math.sqrt(self.offroad_penalty / self.predictions),
+                self.cumul_speed / self.predictions,
+            ])
+
+    def _to_input_array(self, state, smaller=False):
+        array = []
+        array.append(state.angle / 180.0)
+        # TODO - TO ADD BACK IF USING THE CIRCUIT MODEL
+        # array.append(self.damage/10000.0)
+        array.append(state.speed_x / 40.0)
+        array.append(state.speed_y / 40.0)
+        for j in range(0, 19, 3 if smaller else 1):
+            if math.fabs(state.distance_from_center) > 1 or state.distances_from_edge[j] < 0:
+                array.append(-1)
+            else:
+                array.append(state.distances_from_edge[j] / 200.0)
+        array.append(state.distance_from_center)
+
+        # TODO - TO REMOVE FOR THE CIRCUIT MODEL
+        # for j in range(4):
+        #     array.append(self.wheel_velocities[j] / 3000.0)  # /150.0
+        array.append(state.z - 0.36)
+        array.append(state.speed_z / 10.0)
+
+        # TODO - deal with problem that those values are reliable only once every second
+        # for i in range(5):
+        #     array.append(self.focused_distances_from_edge[i]/200.0)
+
+        return np.array(array)
--- run.py
+++ run.py
@@ -1,8 +1,84 @@
-from pytocl.main import main
+from pytocl.protocol import Client
 from driver import Driver
 from rnn_model import RNNModelSteering
+from neat_model import NeatModel
+import argparse
+import signal
+import sys
+import logging
+
+
+model = None
+model_type = ""
+
+
+def sigterm_handler(_signo, _stack_frame):
+    print('Someone killed me')
+    if model_type == 'NEAT' and model is not None:
+        model.saveResults()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, sigterm_handler)
+
+signal.signal(signal.SIGTERM, sigterm_handler)
+
 
 if __name__ == '__main__':
-    main(Driver(
-    	RNNModelSteering("TrainedNNs/steer_norm_aalborg_provided_batch-50.pt"),
-    	logdata=False))
+    parser = argparse.ArgumentParser(
+        description='Model selection for the client for TORCS racing car simulation with SCRC network server.'
+    )
+    parser.add_argument(
+        '-m',
+        '--model',
+        help='Choose model [RNN,NEAT]',
+        default="RNN"
+    )
+    parser.add_argument(
+        '-f',
+        '--model_file',
+        help='Filename to the model file',
+        default="TrainedNNs/steer_norm_aalborg_provided_batch-50.pt"
+    )
+    parser.add_argument(
+        '-o',
+        '--output_file',
+        help='Output file with the results',
+        default="results.txt"
+    )
+    parser.add_argument(
+        '--hostname',
+        help='Racing server host name.',
+        default='localhost'
+    )
+    parser.add_argument(
+        '-p',
+        '--port',
+        help='Port to connect, 3001 - 3010 for clients 1 - 10.',
+        type=int,
+        default=3001
+    )
+    parser.add_argument('-v', help='Debug log level.', action='store_true')
+    args = parser.parse_args()
+
+    model_type = args.model
+    if args.model == 'RNN':
+        model = RNNModelSteering(args.f)
+    elif args.model == 'NEAT':
+        model = NeatModel(args.model_file, args.output_file)
+    print(args.model,model)
+        # switch log level:
+    if args.v:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+    del args.v
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s %(levelname)7s %(name)s %(message)s"
+    )
+    driver = Driver(model=model, logdata=False)
+    print(driver)
+    # start client loop:
+    client = Client(driver=driver, hostname=args.hostname,port=args.port)
+    client.run()
