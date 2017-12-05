6829248e8ea72453b7d7c257ae2ab50edba89703
Lukas Jelinek <lukx19@gmail.com>
2017-12-05 10:08:28 +0100

    Adding fitness keeping car in the middle.

---
+++ fitness_middle.py
@@ -0,0 +1,4 @@
+
+
+def fitness(result, timelimit):
+    return 10*result['avg_speed'][-1] + result['raced_distance'][-1] - 200 - 2000 * result['offroad_penalty'][-1] - 0.1 * result['damage'][-1] - 800*result['distance_from_center']
